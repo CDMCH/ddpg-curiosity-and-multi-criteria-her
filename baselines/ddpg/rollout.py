ea3ec34c50e891392ac6871cdad872b2c5f9463e
John Doe <johndoe@notrealemail.com>
2019-03-10 18:13:34 -0700

    fix import error

--- baselines/ddpg/rollout.py
+++ baselines/ddpg/rollout.py
@@ -4,14 +4,55 @@ import numpy as np
 import pickle
 import os
 import cv2
+import inspect
+import functools
 
-from baselines.her.util import convert_episode_to_batch_major, store_args
 from baselines import logger
 
 try:
     from mujoco_py import MujocoException
 except Exception:
     logger.warn("Mujoco could not be imported.")
+
+
+def convert_episode_to_batch_major(episode):
+    """Converts an episode to have the batch dimension in the major (first)
+    dimension.
+    """
+    episode_batch = {}
+    for key in episode.keys():
+        val = np.array(episode[key]).copy()
+        # make inputs batch-major instead of time-major
+        episode_batch[key] = val.swapaxes(0, 1)
+
+    return episode_batch
+
+
+def store_args(method):
+    """Stores provided method args as instance attributes.
+    """
+    argspec = inspect.getfullargspec(method)
+    defaults = {}
+    if argspec.defaults is not None:
+        defaults = dict(
+            zip(argspec.args[-len(argspec.defaults):], argspec.defaults))
+    if argspec.kwonlydefaults is not None:
+        defaults.update(argspec.kwonlydefaults)
+    arg_names = argspec.args[1:]
+
+    @functools.wraps(method)
+    def wrapper(*positional_args, **keyword_args):
+        self = positional_args[0]
+        # Get default arg values
+        args = defaults.copy()
+        # Add provided arg values
+        for name, value in zip(arg_names, positional_args[1:]):
+            args[name] = value
+        args.update(keyword_args)
+        self.__dict__.update(args)
+        return method(*positional_args, **keyword_args)
+
+    return wrapper
 
 
 class RolloutWorker:
